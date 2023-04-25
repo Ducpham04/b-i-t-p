fb54485a6e093f55cf45f46449365fba4cb74560
Đức Phạm Văn <pvanduc0403@gmail.com>
2023-04-25 17:23:00 +0700

    homework

---
+++ java1.js
@@ -0,0 +1,9 @@
+let duc = document.getElementById("hello")
+duc.innerHTML = "<strong style='color :red' > xin chào các bé neh </strong>"
+console.log(duc)
+let van = document.getElementById("van") 
+document.getElementById("pass").onclick = funtion() ;  { 
+    if(van.getAttribute("type")== "password");
+    else if (van.getAttribute("type")== "text")
+    van.setAttribute("type", "password");
+}
