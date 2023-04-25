0c2512b3f11865e3d8215b46c6b2145a60b88d5d
Đức Phạm Văn <pvanduc0403@gmail.com>
2023-04-25 17:57:35 +0700

    baitap3

--- java1.js
+++ java1.js
@@ -1,9 +1,4 @@
-let duc = document.getElementById("hello")
-duc.innerHTML = "<strong style='color :red' > xin chào các bé neh </strong>"
-console.log(duc)
-let van = document.getElementById("van") 
-document.getElementById("pass").onclick = funtion() ;  { 
-    if(van.getAttribute("type")== "password");
-    else if (van.getAttribute("type")== "text")
-    van.setAttribute("type", "password");
+
+function function1 (){
+    alert ('xin chào các bạn') ;
 }
