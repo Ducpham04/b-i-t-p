6e949962e8cca0b3d7212f628ab522d34f9096cd
Phạm Văn Đức <130365309+Ducpham04@users.noreply.github.com>
2023-04-25 17:58:49 +0700

    Delete java1.js

--- java1.js
+++
@@ -1,9 +0,0 @@
-let duc = document.getElementById("hello")
-duc.innerHTML = "<strong style='color :red' > xin chào các bé neh </strong>"
-console.log(duc)
-let van = document.getElementById("van") 
-document.getElementById("pass").onclick = funtion() ;  { 
-    if(van.getAttribute("type")== "password");
-    else if (van.getAttribute("type")== "text")
-    van.setAttribute("type", "password");
-}
