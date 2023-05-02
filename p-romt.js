47387fb7352c425f3909d90ec1ea322917bea2c5
Phạm Văn Đức <130365309+Ducpham04@users.noreply.github.com>
2023-05-02 09:07:04 +0700

    Toán tử

---
+++ p-romt.js
@@ -0,0 +1,8 @@
+let inputWidth ;
+let inputHeight ;
+inputWidth = prompt("Enter the Width");
+inputHeight = promt("Enter the Heigth");
+let Width = parseInt(inputWidth);      
+let Heigth = parseInt(inputHeight);
+let area = Width * Heigth ;
+document.write("The area is: " + area);
